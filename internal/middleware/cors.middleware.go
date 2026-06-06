0eca713018419cef600404e134463892c85d26c8
anggavb <anggavb8@gmail.com>
2026-06-06 16:58:18 +0700

    feat(middleware): add CORS middleware to handle cross-origin requests

---
+++ internal/middleware/cors.middleware.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"net/http"
+	"slices"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CORSMiddleware(ctx *gin.Context) {
+	allowedOrigin := []string{
+		"http://localhost:5173",
+		"http://127.0.0.1:5173",
+	}
+	currentOrigin := ctx.GetHeader("Origin")
+	if slices.Contains(allowedOrigin, currentOrigin) {
+		ctx.Header("Access-Control-Allow-Origin", currentOrigin)
+		ctx.Header("Access-Control-Allow-Credentials", "true")
+	}
+
+	allowedHeaders := []string{"Content-Type", "Authorization", "X-Reset-Token"}
+	ctx.Header("Access-Control-Allow-Headers", strings.Join(allowedHeaders, ", "))
+
+	allowedMethods := []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodPatch, http.MethodOptions}
+	ctx.Header("Access-Control-Allow-Methods", strings.Join(allowedMethods, ", "))
+
+	if ctx.Request.Method == http.MethodOptions {
+		ctx.AbortWithStatus(http.StatusNoContent)
+		return
+	}
+
+	ctx.Next()
+}
--- internal/router/main.route.go
+++ internal/router/main.route.go
@@ -6,9 +6,12 @@ import (
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	_ "github.com/tickitz-backend/docs"
+	"github.com/tickitz-backend/internal/middleware"
 )
 
 func InitRouter(router *gin.Engine, db *pgxpool.Pool) {
+	router.Use(middleware.CORSMiddleware)
+
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	RegisterAuthRouter(router, db)
