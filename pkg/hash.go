af11a610f45c85d1fafe6fcf5ba14637c6ffb44e
nopalllfd <Naufalfadhilah689@gmail.com>
2026-06-08 12:27:14 +0700

    chore: delete unused logging

--- pkg/hash.go
+++ pkg/hash.go
@@ -49,8 +49,6 @@ func (h *HashConfig) Hash(pwd string) string {
 	salt := h.genSalt()
 	hash := argon2.IDKey([]byte(pwd), salt, h.Time, h.Memory, h.Threds, h.KeyLen)
 
-	// format hash
-	// $argon2id$v=$m=,t=,p=$salt$hash
 	version := argon2.Version
 	encodedSalt := base64.RawStdEncoding.EncodeToString(salt)
 	encodedHash := base64.RawStdEncoding.EncodeToString(hash)
@@ -64,41 +62,34 @@ func (h *HashConfig) Compare(pwd string, hashedPwd string) error {
 
 	// cek panjang
 	log.Println(splittedHash, len(splittedHash))
-	log.Println("masuk invalid hash")
 	if len(splittedHash) != 6 {
 		return errors.New("invalid Hash")
 	}
 
 	// cek argon2id
-	log.Println("masuk argon2id salah")
 	if splittedHash[1] != "argon2id" {
 		return errors.New("not an argon2id hash")
 	}
-	log.Println("wrong ssca")
 	var version int
 	if _, err := fmt.Sscanf(splittedHash[2], "v=%d", &version); err != nil {
 		return errors.New("wrong sscanf syntax")
 	}
 
-	log.Println("wrong versi argon")
 	if version != argon2.Version {
 		return errors.New("wrong argon2id version")
 	}
 
 	var memory, time uint32
 	var threads uint8
-	log.Println("wrong sscan")
 	if _, err := fmt.Sscanf(splittedHash[3], "m=%d,t=%d,p=%d", &memory, &time, &threads); err != nil {
 		return errors.New("wrong sscanf syntax")
 	}
 
 	// ambil salt dan hash
-	log.Println("wrong decode salt")
 	salt, err := base64.RawStdEncoding.DecodeString(splittedHash[4])
 	if err != nil {
 		return errors.New("failed decode salt")
 	}
-	log.Println("wrong decode hash")
 	hash, err := base64.RawStdEncoding.DecodeString(splittedHash[5])
 	if err != nil {
 		return errors.New("failed decode hash")
@@ -106,7 +97,6 @@ func (h *HashConfig) Compare(pwd string, hashedPwd string) error {
 	// generate hash from incoming password (input user passwd)
 	newHash := argon2.IDKey([]byte(pwd), salt, time, memory, threads, uint32(len(hash)))
 
-	log.Println("wrong decode pw")
 	if subtle.ConstantTimeCompare(hash, newHash) == 0 {
 		return errors.New("wrong password")
 	}
