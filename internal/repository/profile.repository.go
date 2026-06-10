ca7ebb9da62c5adccf05485b95c508a99e0d34f0
nopalllfd <Naufalfadhilah689@gmail.com>
2026-06-10 17:42:34 +0700

    feat: add default null value

--- internal/repository/profile.repository.go
+++ internal/repository/profile.repository.go
@@ -22,7 +22,7 @@ func NewProfileRepository(db *pgxpool.Pool) *ProfileRepository {
 }
 
 func (r *ProfileRepository) GetProfile(ctx context.Context, id int) (model.UserProfile, error) {
-	sql := `SELECT COALESCE(u.firstname, '') AS first_name, COALESCE(u.lastname, '') AS last_name, u.phone_number AS phone, u.photo, l.name AS loyalty_tier FROM users u JOIN loyalty_tiers l ON l.id = u.loyalty_tier_id WHERE u.id = $1`
+	sql := `SELECT COALESCE(u.firstname, '') AS first_name, COALESCE(u.lastname, '') AS last_name, COALESCE(u.phone_number, '') AS phone, COALESCE(u.photo, ''), l.name AS loyalty_tier FROM users u JOIN loyalty_tiers l ON l.id = u.loyalty_tier_id WHERE u.id = $1`
 	var profile model.UserProfile
 	if err := r.db.QueryRow(ctx, sql, id).Scan(&profile.FirstName, &profile.LastName, &profile.Phone, &profile.Photo, &profile.LoyaltyTier); err != nil {
 		return model.UserProfile{}, err
