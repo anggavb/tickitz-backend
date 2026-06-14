9edecd622ff479f35185b7d72c4a35acf0c3bd6a
anggavb <anggavb8@gmail.com>
2026-06-14 21:03:23 +0700

    feat(seeds): add movie cinemas, orders, and order details seeders; remove old orders seeder and README; add poster assets

--- db/seeds/013_movie_cinemas_seeder.sql
+++ db/seeds/013_movie_cinemas_seeder.sql
@@ -1,14 +1,3 @@
-INSERT INTO showtimes (id, showtime) VALUES
-  (1, '08:30:00'),
-  (2, '10:00:00'),
-  (3, '14:30:00'),
-  (4, '19:30:00')
-ON CONFLICT (id) DO UPDATE SET
-  showtime = EXCLUDED.showtime;
-
-SELECT setval(pg_get_serial_sequence('showtimes', 'id'), COALESCE((SELECT MAX(id) FROM showtimes), 1), true);
-
-
 INSERT INTO movie_cinemas (movie_id, cinema_id, show_date, showtime_id, price)
 SELECT
   schedule_templates.movie_id,
@@ -18,10 +7,10 @@ SELECT
   schedule_templates.price
 FROM (
   VALUES
-    (1, 1, '2026-06-10'::date, '2026-06-16'::date, 1, 50000),
-    (2, 1, '2026-06-10'::date, '2026-06-16'::date, 2, 65000),
-    (3, 1, '2026-06-10'::date, '2026-06-16'::date, 3, 80000),
-    (4, 2, '2026-06-10'::date, '2026-06-16'::date, 4, 50000)
+    (1, 1, '2026-06-14'::date, '2026-06-20'::date, 1, 50000),
+    (2, 1, '2026-06-14'::date, '2026-06-20'::date, 2, 65000),
+    (3, 1, '2026-06-14'::date, '2026-06-20'::date, 3, 80000),
+    (4, 2, '2026-06-14'::date, '2026-06-20'::date, 4, 50000)
 ) AS schedule_templates(movie_id, cinema_id, start_date, end_date, showtime_id, price)
 CROSS JOIN LATERAL generate_series(schedule_templates.start_date, schedule_templates.end_date, interval '1 day') AS generated_date
 ON CONFLICT (movie_id, cinema_id, show_date, showtime_id) DO UPDATE SET
---
+++ db/seeds/015_order_details_seeder.sql
@@ -0,0 +1,9 @@
+INSERT INTO "order_details" ("id", "order_id", "seat_id", "showtime_id", "price", "movie_cinema_id") VALUES
+(1, '92fc8ee8-b645-441b-a756-d9a2d14d2a98', 220, 2, 35000, 13),
+(2, '92fc8ee8-b645-441b-a756-d9a2d14d2a98', 221, 2, 35000, 13),
+(3, '92fc8ee8-b645-441b-a756-d9a2d14d2a98', 260, 2, 35000, 13),
+(4, '92fc8ee8-b645-441b-a756-d9a2d14d2a98', 261, 2, 35000, 13),
+(5, '938acaf6-fa7b-4d33-8fd9-5e0156ca534f', 259, 3, 35000, 19),
+(6, '938acaf6-fa7b-4d33-8fd9-5e0156ca534f', 258, 3, 35000, 19),
+(7, 'e43a8a52-caa6-494b-beb4-e4fdffe5569c', 253, 3, 35000, 19),
+(8, 'e43a8a52-caa6-494b-beb4-e4fdffe5569c', 254, 3, 35000, 19);
