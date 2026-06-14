39e2a51535dab39575b8fd9de82ac459657ea315
anggavb <anggavb8@gmail.com>
2026-06-14 23:04:14 +0700

    feat(seeds): update order and order details seeders with new entries

--- db/seeds/015_order_details_seeder.sql
+++ db/seeds/015_order_details_seeder.sql
@@ -1,9 +1,5 @@
-INSERT INTO "order_details" ("id", "order_id", "seat_id", "showtime_id", "price", "movie_cinema_id") VALUES
-(1, '92fc8ee8-b645-441b-a756-d9a2d14d2a98', 220, 2, 35000, 13),
-(2, '92fc8ee8-b645-441b-a756-d9a2d14d2a98', 221, 2, 35000, 13),
-(3, '92fc8ee8-b645-441b-a756-d9a2d14d2a98', 260, 2, 35000, 13),
-(4, '92fc8ee8-b645-441b-a756-d9a2d14d2a98', 261, 2, 35000, 13),
-(5, '938acaf6-fa7b-4d33-8fd9-5e0156ca534f', 259, 3, 35000, 19),
-(6, '938acaf6-fa7b-4d33-8fd9-5e0156ca534f', 258, 3, 35000, 19),
-(7, 'e43a8a52-caa6-494b-beb4-e4fdffe5569c', 253, 3, 35000, 19),
-(8, 'e43a8a52-caa6-494b-beb4-e4fdffe5569c', 254, 3, 35000, 19);
+INSERT INTO "order_details" ("order_id", "seat_id", "showtime_id", "price", "movie_cinema_id") VALUES
+('55a8f2b8-f152-4e28-b876-64f12c077b7d', 260, 3, 80000, 16),
+('55a8f2b8-f152-4e28-b876-64f12c077b7d', 261, 3, 80000, 16),
+('ef431f0e-dcfe-4721-9958-5623633d3096', 220, 2, 65000, 9),
+('ef431f0e-dcfe-4721-9958-5623633d3096', 221, 2, 65000, 9);
